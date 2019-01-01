66043e4a02f9912c8cb7f079bd0f039bb5408d57
metacoin <support@colorcoin.org>
2019-01-01 19:54:34 +0000

    added code to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
+import {DaemonApi} from 'js-oip';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    let api = new DaemonApi();
+    const test = async () => {
+        let txid = 'cc9a11050acdc4401aec3f40c4cce123d99c0f2c27d4403ae4a2536ee38a4716';
+        let {success, error, artifact} = await api.getArtifact(txid);
+        return artifact;
+    }
+    test().then((art) => {console.log('artifact: ', art)}).catch(err => {console.error('err: ', err)})
+    
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        Hello world!
         </header>
       </div>
     );
